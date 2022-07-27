9a243246a5479069875458c37eb7a53214c7bcf0
Radim Sückr <radim.suckr@gmail.com>
2022-07-27 14:46:37 +0200

    initial commit

---
+++ Formula/bindfs.rb
@@ -0,0 +1,49 @@
+class Bindfs < Formula
+  desc "FUSE file system for mounting to another location"
+  homepage "https://bindfs.org/"
+  url "https://bindfs.org/downloads/bindfs-1.17.0.tar.gz"
+  sha256 "70da57d49e7794fe54b8575bfdd6a7943aab54ada2e8e2fdf4be04e0011451dc"
+  license "GPL-2.0-or-later"
+
+  head do
+    url "https://github.com/mpartel/bindfs.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+
+    if build.head?
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
+
+    system "make", "install"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        The reasons for disabling this formula can be found here:
+          https://github.com/Homebrew/homebrew-core/pull/64491
+
+        An external tap may provide a replacement formula. See:
+          https://docs.brew.sh/Interesting-Taps-and-Forks
+      EOS
+    end
+  end
+
+  test do
+    system "#{bin}/bindfs", "-V"
+  end
+end
