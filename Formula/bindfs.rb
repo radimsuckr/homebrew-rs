68d266cb67ea16d2362316e534271260429bc5de
Radim Sückr <radim.suckr@gmail.com>
2022-07-27 14:58:36 +0200

    tmp

--- Formula/bindfs.rb
+++ Formula/bindfs.rb
@@ -5,13 +5,13 @@ class Bindfs < Formula
   sha256 "70da57d49e7794fe54b8575bfdd6a7943aab54ada2e8e2fdf4be04e0011451dc"
   license "GPL-2.0-or-later"
 
-  head do
-    url "https://github.com/mpartel/bindfs.git"
+  # head do
+  #   url "https://github.com/mpartel/bindfs.git"
 
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
+  #   depends_on "autoconf" => :build
+  #   depends_on "automake" => :build
+  #   depends_on "libtool" => :build
+  # end
 
   depends_on "pkg-config" => :build
 
@@ -19,14 +19,9 @@ class Bindfs < Formula
     args = %W[
       --disable-debug
       --disable-dependency-tracking
-      --prefix=#{prefix}
     ]
 
-    if build.head?
-      system "./autogen.sh", *args
-    else
-      system "./configure", *args
-    end
+    system "./configure", *args
 
     system "make", "install"
   end
